5fa58586f6afd80755ff735dd98ff2717120f177
Jaime Perez Varas <jeperezva@falabella.cl>
2019-12-05 12:30:33 -0300

    Avances Capitulo 2

--- 01_02_Mis_Picadas/app/src/main/java/cl/jaimikus/android/mispicadas/MainActivity.java
+++ 01_02_Mis_Picadas/app/src/main/java/cl/jaimikus/android/mispicadas/MainActivity.java
@@ -2,13 +2,35 @@ package cl.jaimikus.android.mispicadas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Hace referencia a la vista que esta en la carpeta de Layout
         setContentView(R.layout.activity_main);
+
+        //Simular una tarea de 3 segundos
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                //Se usa la clase Intent para abrir otra pantalla
+                Intent abir_pantalla = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(abir_pantalla);
+                //Y finalizo la actividad actual
+                finish();
+            }
+
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, 3000); //3 segundos de duracion
+
     }
 }
